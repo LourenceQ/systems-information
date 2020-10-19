3043e912416c6e8a69a4560ef5580b814cb0fcdd
Louernce <lqfmecatronica@gmail.com>
2020-10-19 18:44:24 -0300

    Implemented {% extends %} template  tag

--- si_pro/si_app/views.py
+++ si_pro/si_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'si_app/base.html')
+    return render(request,'si_app/index.html')
 
 def algo(request):
     return render(request,'si_app/algo.html')
@@ -12,9 +12,6 @@ def arq(request):
 
 def ing(request):
     return render(request,'si_app/ingles.html')
-
-def proj(request):
-    return render(request,'si_app/projetos.html')
 
 def proj(request):
     return render(request,'si_app/projetos.html')
